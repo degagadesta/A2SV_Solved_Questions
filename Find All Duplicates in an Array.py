aea3b60a33b143cb88272ebd668d2a637fda1418
Dagaga Desta <dagagadasta09@gmail.com>
2026-02-05 16:40:17 +0300

    Add findDuplicates method to Solution class

---
+++ Find All Duplicates in an Array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        res=[]
+        cnt =1
+        nums.sort()
+        i=0
+        while i < len(nums)-1:
+            if nums[i]==nums[i+1]:
+                res.append(nums[i])
+                i+=2
+            else:
+                i+=1    
+        return res        
+
+        
