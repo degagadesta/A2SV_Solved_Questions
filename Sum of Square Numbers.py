91e62dc1857450379dffdce0ccf4d0f40f1d81e8
Dagaga Desta <dagagadasta09@gmail.com>
2026-02-24 19:09:05 +0300

    Create Sum of Square Numbers.py

---
+++ Sum of Square Numbers.py
@@ -0,0 +1,17 @@
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        left = 0
+        right = int((c ** 0.5))
+        
+        while left <= right :
+            total = left ** 2 + right ** 2
+            if total == c :
+                return True
+            elif left ** 2 + right ** 2 < c :
+                left += 1
+            else:
+                right -= 1
+        return False                
+
+
+        
