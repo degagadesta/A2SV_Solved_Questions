4539c18ddfed4bd75f1f6ea08ba54f76e1470128
Dagaga Desta <dagagadasta09@gmail.com>
2026-04-22 22:44:01 +0300

    Add solution for Shortest Completing Word

---
+++ leetcode/0748-shortest-completing-word/0748-shortest-completing-word.py
@@ -0,0 +1,10 @@
+class Solution:
+    def shortestCompletingWord(self, licensePlate: str, words: List[str]) -> str:
+        lp = collections.Counter(c for c in licensePlate.lower() if c.isalpha())
+        ans = None
+        for w in words:
+            cw = collections.Counter(w)
+            if all(cw.get(c, 0) >= lp[c] for c in lp):
+                if ans is None or len(w) < len(ans):
+                    ans = w
+        return ans
