3ddd642e517b5d478c015adb8c97b9de80349b20
Dagaga Desta <dagagadasta09@gmail.com>
2026-05-13 19:57:41 +0300

    Add solution for Sort Colors

--- leetcode/0075-sort-colors/0075-sort-colors.py
+++ leetcode/0075-sort-colors/0075-sort-colors.py
@@ -3,19 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        left = 0 
-        right = len(nums) - 1
-        read = 0
-        while read <= right :
-            if nums[read] == 0:
-                nums[left] , nums[read] = nums[read], nums[left]
-                left += 1
-                read += 1
-            elif nums[read] == 2:
-                nums[right], nums[read] = nums[read] , nums[right] 
-                right -= 1
-            else:
-                read += 1    
+        # left ,right,read = 0,len(nums) - 1, 0
+        # while read <= right :
+        #     if nums[read] == 0:
+        #         nums[left] , nums[read] = nums[read], nums[left]
+        #         left += 1
+        #     elif nums[read] == 2:
+        #         nums[right], nums[read] = nums[read] , nums[right] 
+        #         right -= 1
+        #         read -= 1
+        #     read += 1
+        nums.sort()
                
 
         
