eea569155eb891cb0b81a4c699eb46f0655b264b
Dagaga Desta <dagagadasta09@gmail.com>
2026-05-20 13:06:16 +0300

    Add solution for Longest Common Subsequence

--- leetcode/1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ leetcode/1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,16 +1,24 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        memo = [[-1]*(len(text2)) for _ in range(len(text1))]
-        def dp(i,j):
-            if i == len(text1) or j == len(text2):
-                return 0
-            if memo[i][j] == -1:    
-                if  text1[i] == text2[j]:
-                    memo[i][j] = 1 + dp(i+1,j+1)
+        m , n = len(text1),len(text2)
+        dp = [[0]*(n+1) for _ in range(m+1)]
+        for i in range(m-1,-1,-1):
+            for j in range(n-1,-1,-1):
+                if text1[i] == text2[j]:
+                    dp[i][j] = 1 + dp[i+1][j+1]
                 else:
-                    memo[i][j] = max(dp(i,j+1), dp(i+1,j))    
-            return memo[i][j]
-        return dp(0,0)    
+                    dp[i][j] = max(dp[i][j+1], dp[i+1][j])
+        return dp[0][0]                
+        # def dp(i,j):
+        #     if i == len(text1) or j == len(text2):
+        #         return 0
+        #     if memo[i][j] == -1:    
+        #         if  text1[i] == text2[j]:
+        #             memo[i][j] = 1 + dp(i+1,j+1)
+        #         else:
+        #             memo[i][j] = max(dp(i,j+1), dp(i+1,j))    
+        #     return memo[i][j]
+        # return dp(0,0)    
 
 
 
